48bc25fbe25af0b3674d4f27572f79c1c7d6b773
Juan Rossi <juan@getmango.com>
2015-09-02 19:34:29 -0300

    Fixed bug where tweets were not being created

--- traealfajores.py
+++ traealfajores.py
@@ -1,3 +1,5 @@
+import json
+
 from tweepy.streaming import StreamListener
 from tweepy import Stream
 from twitterapi import TwitterAPI
@@ -10,12 +12,16 @@ class TraeAlfajoreListener(StreamListener):
     A listener handles to handle tweets from Ezeiza
     """
     def on_data(self, data):
-        twitter.tweet_text('Trae alfajores!', data.get('id'))
+        twitter.tweet_text('Trae alfajores!', json.loads(data).get('id'))
         return True
 
     def on_error(self, status):
         if status == 420:
             return False
+
+    def on_timeout(self):
+        print 'Timeout...'
+        return True  # Don't kill the stream
 
 
 if __name__ == '__main__':
